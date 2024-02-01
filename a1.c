32fb936f6cb54ba4e281013e87c3cdaca5907601
Sam Holtzman <sam.wolf.holtzman@gmail.com>
2024-02-01 12:57:15 -0800

    Initial commits + isLetter() function

---
+++ a1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int isLetter(char ch)
+{
+    return ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'));
+}
+
+int main()
+{
+    const char *inputFile = "input.txt";
+    const char *outputFile = "output.txt";
+
+    return 0;
+
+}
