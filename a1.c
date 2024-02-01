01464c9dbae2a191d9cbb143d4e5e09a53edf619
Sam Holtzman <sam.wolf.holtzman@gmail.com>
2024-02-01 13:33:53 -0800

    readfile and main base functions

--- a1.c
+++ a1.c
@@ -7,6 +7,22 @@ int isLetter(char ch)
     return ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'));
 }
 
+char *readFile(const char *inputFileName) {
+    FILE *inputFile = fopen(inputFileName, "r");
+    
+    if (inputFile == NULL) {
+
+        perror(stderr, "Invalid input data. File empty or unfound.");
+        exit(EXIT_FAILURE);
+
+    }
+    
+
+    fclose(inputFile);
+
+    return result;
+}
+
 int main()
 {
     const char *inputFile = "input.txt";
@@ -15,3 +31,4 @@ int main()
     return 0;
 
 }
+
